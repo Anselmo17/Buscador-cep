eff9ba0085b14d6a978c64f433753cc70fd2cf42
Anselmo <ansinftec17@gmail.com>
2024-01-05 10:34:24 -0300

    FEAT:Adicionando retorno dos dados em variavel cep

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import {
+  HttpClient
+} from '@angular/common/http';
+import { Cep } from './models/cep.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app-cep';
+
+  public title = 'app-cep';
+  public cepFound!: Cep;
+  input = new FormControl('');
+
+  constructor(
+    private http: HttpClient
+  ) {
+    this.input.valueChanges.subscribe(valor => {
+      console.log('-------valor--------', valor)
+      if (valor.length < 8) return;
+
+      this.pesquisa(valor).subscribe(cep => {
+        this.cepFound = cep;
+        console.log('----- cep retornado -------', this.cepFound);
+      });
+    }
+    );
+  }
+
+  pesquisa(cep: string) {
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
+    return this.http.get<any>(url);
+  }
+
+  isNumber(evt: any) {
+    evt = (evt) ? evt : window.event;
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+      return false;
+    }
+    return true;
+  }
+
+  cepFormated(cep: string) {
+    const cepPart1 = cep.substring(0, 5);
+    const cepPart2 = cep.substring(5);
+
+    return cepPart1 + '-' + cepPart2;
+  }
 }
